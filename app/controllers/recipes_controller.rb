88e0cca382cb2630866fa7b77a0c3106f2da90f5
Marta Aparicio <martaaparicio14@gmail.com>
2026-03-30 20:38:27 +0200

    changes views and controllers

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,26 @@
 class RecipesController < ApplicationController
+
+  def index
+    @recipes = Recipe.all
+  end
+
+  def create
+    # TODO change it to save recipes with chatbot?
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to recipes_path, notice: "Recipe created successfully!"
+    else
+      render :new
+    end
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :ingredients, :description, :keywords, :calories, :allergens)
+  end
 end
