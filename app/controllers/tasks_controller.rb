336841ffd46569c543c50effa36f616fd4dc8774
Marie-Laure Seguin <mlseguin@gmail.com>
2026-03-31 18:32:17 +0200

    adding controller method and views for tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,44 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
     @role = current_user.role
-    @family = current_user.family
     @user_tasks = current_user.tasks
-    @family_tasks = @family.tasks
+    @family_tasks = current_user.family.tasks
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_restaurant
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(
+      :name,
+      :description,
+      :start_date,
+      :end_date,
+      :task_points,
+      :frequency
+    )
   end
 end
