afdc2ba27e5022edde241d3c4b791f64050e88c9
afaf kharsa <72041457+Afafkharsa@users.noreply.github.com>
2026-03-31 22:06:02 +0400

    fixed bugs

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :tasks
+  has_many :tasks, dependent: :destroy
   belongs_to :family, optional: true
   has_one_attached :photo
   has_many :rewards, dependent: :destroy
