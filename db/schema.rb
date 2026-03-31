cc36b4e6f0d2817a80d4274d55f85498c60aca6f
Marta Aparicio <martaaparicio14@gmail.com>
2026-03-31 20:17:14 +0200

    form for add recipes and search bar

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_03_31_130323) do
+ActiveRecord::Schema[7.1].define(version: 2026_03_31_114400) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -123,8 +123,6 @@ ActiveRecord::Schema[7.1].define(version: 2026_03_31_130323) do
     t.datetime "updated_at", null: false
     t.bigint "task_template_id"
     t.bigint "user_id", null: false
-    t.bigint "family_id", null: false
-    t.index ["family_id"], name: "index_tasks_on_family_id"
     t.index ["task_template_id"], name: "index_tasks_on_task_template_id"
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
@@ -154,7 +152,6 @@ ActiveRecord::Schema[7.1].define(version: 2026_03_31_130323) do
   add_foreign_key "recipe_meal_plans", "meal_plans"
   add_foreign_key "recipe_meal_plans", "recipes"
   add_foreign_key "rewards", "users"
-  add_foreign_key "tasks", "families"
   add_foreign_key "tasks", "task_templates"
   add_foreign_key "tasks", "users"
   add_foreign_key "users", "families"
