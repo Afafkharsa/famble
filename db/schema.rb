78dd212df63f4c7359f08f6b82d21595e027553f
Marta Aparicio <martaaparicio14@gmail.com>
2026-04-07 22:13:12 +0200

    chatbot llm added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_04_07_185847) do
+ActiveRecord::Schema[7.1].define(version: 2026_04_07_190829) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -68,6 +68,7 @@ ActiveRecord::Schema[7.1].define(version: 2026_04_07_185847) do
     t.bigint "family_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "meal"
     t.index ["family_id"], name: "index_meal_plans_on_family_id"
   end
 
