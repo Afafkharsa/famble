557672769472979ed3071c288450047f462ddf96
Marie-Laure Seguin <mlseguin@gmail.com>
2026-04-08 20:30:14 +0200

    restoring unsaved changes for new and edig task templates actions

--- app/controllers/task_templates_controller.rb
+++ app/controllers/task_templates_controller.rb
@@ -1,10 +1,9 @@
 class TaskTemplatesController < ApplicationController
   before_action :set_task_template, only: [:show, :edit, :update, :destroy]
-  before_action :set_family, only: [:show, :update, :destroy]
+  before_action :set_family, only: [:index, :show, :update, :destroy]
 
   def index
     @role = current_user.role
-    @familly = current_user.family
     @generic_templates = TaskTemplate.all.where("family_id IS NULL")
     @family_templates = current_user.family.task_templates
   end
--- config/routes.rb
+++ config/routes.rb
@@ -13,14 +13,14 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
-  resources :recipes, only: [:index, :new, :create, :show, :destroy]
+  resources :recipes
 
   resources :chats, only: [:index, :create, :show] do
     resources :messages, only: [:create]
   end
 
   resources :tasks
-  resources :task_templates, only: [:index, :show, :destroy, :edit, :update]
+  resources :task_templates
 
   resources :reward_templates, only: [:index, :new, :create, :destroy]
 
@@ -35,8 +35,6 @@ Rails.application.routes.draw do
   resources:families do
     resources :member, only: [:index, :show, :new, :create]
     end
-
-    resources :task_templates, only: [:new, :create]
 
   resources :recipe_meal_plans, only: [:destroy, :edit, :update]
 
