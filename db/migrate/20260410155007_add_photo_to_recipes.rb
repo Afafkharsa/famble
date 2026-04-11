b394ebb62b6ccc5e1010237bdc70a482de4ef98c
Marta Aparicio <martaaparicio14@gmail.com>
2026-04-11 09:54:21 +0200

    fix edti button

--- db/migrate/20260410155007_add_photo_to_recipes.rb
+++
@@ -1,5 +0,0 @@
-class AddPhotoToRecipes < ActiveRecord::Migration[7.1]
-  def change
-    add_column :recipes, :photo, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_04_10_155007) do
+ActiveRecord::Schema[7.1].define(version: 2026_04_07_190829) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -100,7 +100,6 @@ ActiveRecord::Schema[7.1].define(version: 2026_04_10_155007) do
     t.string "allergens"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "photo"
   end
 
   create_table "reward_templates", force: :cascade do |t|
