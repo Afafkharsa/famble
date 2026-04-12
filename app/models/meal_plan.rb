737bf0baf08a1eb08478721e388e7a7c81527222
Marie-Laure Seguin <mlseguin@gmail.com>
2026-04-12 19:36:22 +0200

    adding validation parameters to meal plans

--- app/models/meal_plan.rb
+++ app/models/meal_plan.rb
@@ -3,4 +3,7 @@ class MealPlan < ApplicationRecord
   has_many :recipe_meal_plans, dependent: :destroy
   has_many :recipes, through: :recipe_meal_plans
   accepts_nested_attributes_for :recipe_meal_plans, allow_destroy: true
+
+  validates :meal, presence: true
+  validates :day, presence: true
 end
