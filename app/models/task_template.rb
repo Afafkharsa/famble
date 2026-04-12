40573e474ebff72668622b0f722bf30ca6404aa9
Marie-Laure Seguin <mlseguin@gmail.com>
2026-04-12 19:46:30 +0200

    adding validation parameters to task templates

--- app/models/task_template.rb
+++ app/models/task_template.rb
@@ -1,4 +1,7 @@
 class TaskTemplate < ApplicationRecord
   has_many :tasks
   belongs_to :family, optional: true
+
+  validates :name, presence: true, length: { minimum: 2 }
+  validates :task_points, numericality: { greater_than_or_equal_to: 1 }
 end
