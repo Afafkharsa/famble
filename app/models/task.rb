70bec01a28b3c54b69cbea951b852b355f136e12
Marie-Laure Seguin <mlseguin@gmail.com>
2026-04-12 19:45:43 +0200

    add validation parameters to tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,8 @@ class Task < ApplicationRecord
   belongs_to :user
   belongs_to :task_template, optional: true
   has_one :family, through: :user
+
+  validates :name, presence: true, length: { minimum: 2 }
+  validates :task_points, numericality: { greater_than_or_equal_to: 1 }
+  validates :end_date, comparison: { greater_than: :start_date }
 end
