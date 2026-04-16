72b15a86c1aa47486d81f3cfb0fc785ffcab88f6
Wiraphat Kwankaew <wiraphat.k@hotmail.com>
2026-04-16 23:47:11 +0200

    Change style on family

--- app/controllers/families_controller.rb
+++ app/controllers/families_controller.rb
@@ -1,7 +1,7 @@
 class FamiliesController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_members
-  before_action :set_users
+  # before_action :set_members
+  # before_action :set_users
 
  def index
    @family = current_user.family
@@ -13,29 +13,32 @@ class FamiliesController < ApplicationController
  end
 
  def show
+  @family = current_user.family
   @member = User.find(params[:id])
-  # @members = @family.users
+  #@members = @family.users
   render layout: false
  end
 
  def new
+   @family = current_user.family
    @member = User.new
    render layout: false
  end
 
  def create
-   # @member = User.new(params[:id])
-   @member = User.find_by(email:params[:user])
+   @family = current_user.family
+   @member = User.find_by(email:params[:user][:email])
    @family = current_user.family
     if @member
-      if @member.update(family: @family)
+      if @member.update(user_params.merge(family: @family))
         redirect_to families_path, notice: "Added #{@member.name} to family"
       else
         redirect_to families_path, alert: "Could not add member"
       end
     else
       # Can not find email "
-      flash[:alert] = "User with email #{params[:email]} not found"
+      flash[:alert] = "User not found"
+      #@member = User.new
       render :new, status: :unprocessable_entity
     end
 
@@ -48,7 +51,7 @@ class FamiliesController < ApplicationController
     @member = User.find(params[:id])
     authorize @member
     if @member.update(user_params)
-      redirect_to families_path
+      redirect_to families_path, notice: "Member updated!"
     else
       render :edit, status: :unprocessable_entity
     end
@@ -63,11 +66,11 @@ class FamiliesController < ApplicationController
   end
 
   def set_family
-    @family = current_user.family.users
+    @family = current_user.family
   end
 
  def user_params
-  params.require(:user).permit(:name, :birthdate, :photo, :role)
+  params.require(:user).permit(:name, :email, :birthdate, :photo, :role)
  end
 
 end
