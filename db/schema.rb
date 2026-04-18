e13ffd0c177c61bfd10d112916d69808feb4aeb5
Marie-Laure Seguin <mlseguin@gmail.com>
2026-04-18 21:18:44 +0200

    schema changes

--- db/schema.rb
+++ db/schema.rb
@@ -55,6 +55,7 @@ ActiveRecord::Schema[7.1].define(version: 2026_04_18_155809) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["event_id", "user_id"], name: "index_event_participations_on_event_id_and_user_id", unique: true
     t.index ["event_id"], name: "index_event_participations_on_event_id"
     t.index ["user_id"], name: "index_event_participations_on_user_id"
   end
